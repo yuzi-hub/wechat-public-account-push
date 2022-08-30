535edf4b6b747b9906ad842386f443dc4859ac25
yuzi-hub <55371710+yuzi-hub@users.noreply.github.com>
2022-08-30 17:56:50 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ export const config = {
     // 公众号APP_SECRET
     APP_SECRET: "b2715d8cf0d78bd8518cdb768715b50e",
     // 模板消息id
-    TEMPLATE_ID: "VhzcAxK9ZgO2ooSdaULda0oQjHRcgSGY8E5hPsWMLEE",
+    TEMPLATE_ID: "EhbyEXCWugBC5ORTHY43m0WmK-BT7btXrAQ3iUU-3fM",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
     USERS: ["oQKlN5vF6ChDrxGNI246Lo2YYqiQ"],
      
